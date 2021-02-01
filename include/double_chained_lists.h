fc9728f45b1a41d21a8d745c4f851f692cbb82ec
Gasser <antoine.gasser@epitech.eu>
2021-02-01 11:29:51 -0500

    Leak memory resolved

--- include/double_chained_lists.h
+++ include/double_chained_lists.h
@@ -10,6 +10,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /*------------------SECONDARY_LIST------------------*/
 typedef struct l_list l_list;
--- main.c
+++ main.c
@@ -10,12 +10,19 @@
 int main(int ac, char **av)
 {
     list *global = create_global_list(ac, av);
-    l_list *node = malloc(sizeof(l_list));
+    l_list *node = NULL;
 
-    if (global == NULL || node == NULL)
+    if (global == NULL) {
+        write(2, "error memory allocation\n", 25);
         return (84);
-    for (int i = 1; i < ac; i++)
+    }
+    for (int i = 1; i < ac; i++) {
         node = add_to_list(&global, &node, atoi(av[i]));
+        if (node == NULL) {
+            write(2, "error memory allocation\n", 25);
+            return (84);
+        }
+    }
     node = global->first;
     display(node);
     destroy_list(&global, &node);
